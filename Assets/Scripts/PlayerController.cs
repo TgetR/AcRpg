922178a5681879ad1d07d2dac72a900eb12b3b1c
TgetR <123860470+TgetR@users.noreply.github.com>
2025-12-03 22:41:27 +0300

    PlayerController Big update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,12 +12,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] TMP_Text HpText;
     [SerializeField] TMP_Text GoldText;
     private Rigidbody2D _rb;
-    private List<EnemyHealthSystem> _enemyInRadiusList;
+    private HashSet<EnemyHealthSystem> _enemiesInRange;
     private Animator _animator;
     private bool _AttackAllow = true;
     private bool _isInAttack = false;
     private bool _isKnockedBack = false;
     private bool _isKnockBackCooldownEnd = true;
+
+    [SerializeField] private float attackRadius = 1.2f;
+    private CircleCollider2D _attackRangeCollider;
     
 
     //Controlled by StatsManager
@@ -35,8 +39,26 @@ public class PlayerController : MonoBehaviour
 
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-        _enemyInRadiusList = new List<EnemyHealthSystem>();
+        _enemiesInRange = new HashSet<EnemyHealthSystem>();
 
+        SetupAttackRangeTrigger();
+    }
+
+    private void SetupAttackRangeTrigger()
+    {
+        _attackRangeCollider = gameObject.AddComponent<CircleCollider2D>();
+        _attackRangeCollider.isTrigger = true;
+        _attackRangeCollider.radius = attackRadius;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Archer"))
+        {
+            EnemyHealthSystem enemy = collision.GetComponent<EnemyHealthSystem>();
+            _enemiesInRange.Add(enemy);
+            Debug.Log($"Enemy enter to radius. Total enemies in radius: {_enemiesInRange.Count}");
+        }
     }
 
     private void Update()
@@ -47,40 +69,69 @@ public class PlayerController : MonoBehaviour
         _damage = _manager.Damage;
         _Gold = _manager.Gold;
 
-        //Death check
+        UpdateAndCheckStats();
+        AttackCheck();
+    }
+    void UpdateAndCheckStats()
+    {
         HpText.text = "HP: " + _HP + "/" + _maxHP;
         GoldText.text = "Gold: " + _Gold;
         if (_HP <= 0)
         {
             SceneManager.LoadScene("GameOver");
         }
-
-        //Put all enemies in 1.2 circle radius to list
-        Collider2D[] rawResults;
-        rawResults = Physics2D.OverlapCircleAll(transform.position, 1.2f);
-        foreach (Collider2D collider in rawResults)
-        {
-            if (collider.gameObject.CompareTag("Enemy") || collider.gameObject.CompareTag("Archer")) _enemyInRadiusList.Add(collider.gameObject.GetComponent<EnemyHealthSystem>());
-        }
-
-        //Attack check
-        if (Input.GetKey(KeyCode.Mouse0))
+    }
+    void AttackCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             _isInAttack = true;
             //Select attack version
             byte AttackVersion = (byte)Random.Range(0, 2); //0 - first version;  1- second version
             _animator.SetInteger("AttackV", AttackVersion);
-
-            if (_enemyInRadiusList.Count > 0)
+            if (_enemiesInRange.Count > 0)
+            {
+                EnemyHealthSystem closestEnemy = GetClosestEnemy();
+                if (closestEnemy != null)
+                {
+                _animator.SetTrigger("Attack");
+                StartCoroutine(Attack(closestEnemy));
+                }
+            }
+            else
             {
                 _animator.SetTrigger("Attack");
-                StartCoroutine(Attack(_enemyInRadiusList[0]));
+                StartCoroutine(ResetAttackCooldown());
+            } 
+            
+            _enemiesInRange.RemoveWhere(enemy => enemy == null); //Clean up null references
+        }
+    }
+
+    private IEnumerator ResetAttackCooldown()
+    {
+        yield return new WaitForSeconds(0.6f);
+        _isInAttack = false;
+    }
+
+    private EnemyHealthSystem GetClosestEnemy()
+    {
+        EnemyHealthSystem closest = null;
+        float minDistance = float.MaxValue;
+
+        foreach (var enemy in _enemiesInRange)
+        {
+            if (enemy == null) continue;
+
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closest = enemy;
             }
-            else _animator.SetTrigger("Attack");
         }
 
-        //Clear enemy list
-        _enemyInRadiusList.Clear();
+        return closest;
     }
 
     void FixedUpdate()
@@ -148,7 +199,7 @@ public class PlayerController : MonoBehaviour
     IEnumerator Attack(EnemyHealthSystem target)
     {
 
-        if (_AttackAllow)
+        if (_AttackAllow && target != null)
         {
             _AttackAllow = false;
             target.transform.localScale = new Vector3(-FacingDirection, transform.localScale.y);
@@ -160,5 +211,22 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(1);
         _isInAttack = false;
         _AttackAllow = true;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+        
+        Gizmos.color = Color.yellow;
+        if (_enemiesInRange != null)
+        {
+            foreach (var enemy in _enemiesInRange)
+            {
+                if (enemy != null)
+                {
+                    Gizmos.DrawLine(transform.position, enemy.transform.position);
+                }
+            }
+        }
     }
 }

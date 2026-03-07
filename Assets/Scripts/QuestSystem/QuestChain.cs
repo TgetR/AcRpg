58ac893fbd57a3d4182100247f7e324ffa7afa63
TgetR <123860470+TgetR@users.noreply.github.com>
2026-03-07 13:26:31 +0300

    feat: Refactor Type0 quest chain logic and add notifications for quest stages

--- Assets/Scripts/QuestSystem/QuestChain.cs
+++ Assets/Scripts/QuestSystem/QuestChain.cs
@@ -1,60 +1,87 @@
+using System.Collections;
 using UnityEngine;
 
 public class QuestChain : MonoBehaviour
 {
     [SerializeField] private QuestChecker questChecker;
 
-    int Type0Activations = -1; //Type 0 - Enemy kill quest
+    int Type0Activations = 0; //Type 0 - Enemy kill quest
     int Type1Activations = 0; //Type 1 - Collect item quest
     int Type2Activations = 0; //Type 2 - Explore location quest
     //TYPE 1 AND TYPE 2 temporary not used 
 
     bool questTaken = false;
     bool questCompleted = false;
+    bool delayEnd = true;
+    private OnScreenNotify _notify;
+
+    void Start()
+    {
+        _notify = GameObject.FindGameObjectWithTag("Notifyer").GetComponent<OnScreenNotify>();
+    }
+
     public void Type0ChainCheck()
     {
-
-        switch (Type0Activations)
+        if (delayEnd)
         {
-            case -1:
-                if (!questCompleted && !questTaken)
-                {
-                questChecker.TakeQuest("Kill 5 enemies", 1, 5, 50, 50);
-                Type0Activations++;
-                Debug.Log("Type 0 quest taken.");
-                }
-                else if (questCompleted && questTaken)
-                {
-                    questChecker.DeleteQuest();
-                    Debug.Log("Type 0 quest completed and deleted.");
-                }
-                return;
-            case 0:
-                if (!questCompleted && !questTaken)
-                {
-                questChecker.TakeQuest("Kill 10 enemies", 1, 10, 150, 150);
-                Type0Activations++;
-                Debug.Log("Type 0 quest taken. Stage 2");
-                }
-                else if (questCompleted && questTaken)
-                {
-                    questChecker.DeleteQuest();
-                    Debug.Log("Type 0 quest completed and deleted.");
-                }
-                return;
-            case 2:
-                if (!questCompleted && !questTaken)
-                {
-                questChecker.TakeQuest("Kill 20 enemies", 1, 20, 300, 300);
-                Type0Activations++;
-                Debug.Log("Type 0 quest taken.");
-                }
-                else if (questCompleted && questTaken)
-                {
-                    questChecker.DeleteQuest();
-                    Debug.Log("Type 0 quest completed and deleted.");
-                }
-                return;
+            switch (Type0Activations)
+            {
+                case 0:
+                    if (!questCompleted && !questTaken)
+                    {
+                    questChecker.TakeQuest("Kill 5 enemies", 1, 5, 50, 50);
+                    Debug.Log("Type 0 quest taken.");
+                    StartCoroutine(Delay());
+                    }
+
+                    else if (questCompleted && questTaken)
+                    {
+                        Type0Activations++;
+                        questChecker.DeleteQuest();
+                        Debug.Log("Type 0 quest completed and deleted.");
+                        StartCoroutine(Delay());
+                    }
+                    return;
+                case 1:
+                    if (!questCompleted && !questTaken)
+                    {
+                    questChecker.TakeQuest("Kill 10 enemies", 1, 10, 150, 150);
+                    Debug.Log("Type 0 quest taken. Stage 2");
+                    StartCoroutine(Delay());
+                    }
+
+                    else if (questCompleted && questTaken)
+                    {
+                        Type0Activations++;
+                        questChecker.DeleteQuest();
+                        Debug.Log("Type 0 quest completed and deleted.");
+                        StartCoroutine(Delay());
+                    }
+                    return;
+                case 2:
+                    if (!questCompleted && !questTaken)
+                    {
+                    questChecker.TakeQuest("Kill 20 enemies", 1, 20, 300, 300);
+                    Debug.Log("Type 0 quest taken.");
+                    StartCoroutine(Delay());
+                    }
+
+                    else if (questCompleted && questTaken)
+                    {
+                        Type0Activations++;
+                        questChecker.DeleteQuest();
+                        Debug.Log("Type 0 quest completed and deleted.");
+                        StartCoroutine(Delay());
+                    }
+                    return;
+                case 3:
+                    _notify.Notify("No more quest for you! Find new adventures!", 2);
+                    StartCoroutine(Delay()); break;
+                default:
+                    _notify.Notify("ERROR: Invalid quest stage. Try find another quest!", 4);
+                    StartCoroutine(Delay()); break;
+
+            }   
         }
     }
 
@@ -63,5 +90,12 @@ public class QuestChain : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.K)) Debug.Log(Type0Activations);
         questTaken = questChecker.GetQuestTakenStatus();
         questCompleted = questChecker.GetQuestCompletedStatus();
+    }
+
+    IEnumerator Delay()
+    {
+        delayEnd = false;
+        yield return new WaitForSeconds(1f);
+        delayEnd = true;
     }
 }
